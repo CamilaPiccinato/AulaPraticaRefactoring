9e87f535e06f96e9da9dca1ce92164b2d697da5c
CamilaPiccinato <cpvian@minha.fag.edu.br>
2025-11-24 19:23:41 -0300

    Commit 18

--- src/HtmlStatement.java
+++ src/HtmlStatement.java
@@ -1,30 +1,18 @@
-import java.util.Enumeration;
-
 public class HtmlStatement extends Statement {
 
-    public String value(Customer aCustomer) {
-        String result = headerString(aCustomer);
-        Enumeration rentals = aCustomer.getRentals();
-
-        while (rentals.hasMoreElements()) {
-            Rental each = (Rental) rentals.nextElement();
-            result += eachRentalString(each);
-        }
-
-        result += footerString(aCustomer);
-        return result;
-    }
-
-    private String headerString(Customer aCustomer) {
+    @Override
+    protected String headerString(Customer aCustomer) {
         return "<H1>Rentals for <EM>" + aCustomer.getName() + "</EM></H1><P>\n";
     }
 
-    private String eachRentalString(Rental each) {
+    @Override
+    protected String eachRentalString(Rental each) {
         return each.getMovie().getTitle() + ": " +
                 String.valueOf(each.getCharge()) + "<BR>\n";
     }
 
-    private String footerString(Customer aCustomer) {
+    @Override
+    protected String footerString(Customer aCustomer) {
         String result = "";
         result += "<P>You owe <EM>" + aCustomer.getTotalCharge() + "</EM><P>\n";
         result += "On this rental you earned <EM>" +
