c01625393b66eaac20037eda336f99b64b3b55ca
CamilaPiccinato <cpvian@minha.fag.edu.br>
2025-11-24 19:22:22 -0300

    Revert "Commit 17"
    This reverts commit 36a32aa287f675efa0271df914e35f0ff9f06784.

--- src/HtmlStatement.java
+++ src/HtmlStatement.java
@@ -3,35 +3,23 @@ import java.util.Enumeration;
 public class HtmlStatement extends Statement {
 
     public String value(Customer aCustomer) {
-        String result = headerString(aCustomer);
         Enumeration rentals = aCustomer.getRentals();
+        String result = "<H1>Rentals for <EM>" + aCustomer.getName() + "</EM></H1><P>\n";
 
         while (rentals.hasMoreElements()) {
             Rental each = (Rental) rentals.nextElement();
-            result += eachRentalString(each);
+
+            // show figures for this rental
+            result += each.getMovie().getTitle() + ": " +
+                    String.valueOf(each.getCharge()) + "<BR>\n";
         }
 
-        result += footerString(aCustomer);
-        return result;
-    }
-
-    // --- métodos específicos do formato HTML ---
-
-    private String headerString(Customer aCustomer) {
-        return "<H1>Rentals for <EM>" + aCustomer.getName() + "</EM></H1><P>\n";
-    }
-
-    private String eachRentalString(Rental each) {
-        return each.getMovie().getTitle() + ": " +
-                String.valueOf(each.getCharge()) + "<BR>\n";
-    }
-
-    private String footerString(Customer aCustomer) {
-        String result = "";
+        // add footer lines
         result += "<P>You owe <EM>" + String.valueOf(aCustomer.getTotalCharge()) + "</EM><P>\n";
         result += "On this rental you earned <EM>" +
                 String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
                 "</EM> frequent renter points<P>";
+
         return result;
     }
 }
