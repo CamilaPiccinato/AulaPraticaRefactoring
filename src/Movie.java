7c7160fb9ee112e549e40a8ee882292e18a222d7
CamilaPiccinato <cpvian@minha.fag.edu.br>
2025-11-24 19:17:17 -0300

    Commit 11

--- src/Movie.java
+++ src/Movie.java
@@ -1,29 +1,43 @@
 public class Movie {
 
-    public static final int  CHILDRENS = 2;
-    public static final int  REGULAR = 0;
-    public static final int  NEW_RELEASE = 1;
+    public static final int CHILDRENS = 2;
+    public static final int REGULAR = 0;
+    public static final int NEW_RELEASE = 1;
 
     private String _title;
-    private int _priceCode;
+    private Price _price;   // agora é um Price, não mais int
 
     public Movie(String title, int priceCode) {
         _title = title;
-        _priceCode = priceCode;
+        setPriceCode(priceCode);   // delega pra lógica de Price
     }
 
     public int getPriceCode() {
-        return _priceCode;
+        return _price.getPriceCode();
     }
 
     public void setPriceCode(int arg) {
-        _priceCode = arg;
+        switch (arg) {
+            case REGULAR:
+                _price = new RegularPrice();
+                break;
+            case CHILDRENS:
+                _price = new ChildrensPrice();
+                break;
+            case NEW_RELEASE:
+                _price = new NewReleasePrice();
+                break;
+            default:
+                throw new IllegalArgumentException("Incorrect Price Code");
+        }
     }
 
-    public String getTitle (){
+    public String getTitle() {
         return _title;
     }
 
+    // já existiam antes – continuam iguais,
+    // só que agora usam getPriceCode(), que delega para _price
     public double getCharge(int daysRented) {
         double thisAmount = 0;
 
